9d69a6a343cdcdfb773cba46ecd1b3abf51635af
Abbie <abbie@summers.gg>
2021-09-17 15:29:50 +0200

    rendering from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MembersList from ".components/membersList";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Amsterdam Sports Inc.</p>
       </header>
+      <MembersList></MembersList>
     </div>
   );
 }
---
+++ src/components/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to Amsterdam Sports Inc!</h1>
+      <p>Memberships and Sports</p>
+    </main>
+  );
+};
+export default IndexPage;
